b206ac7fbc117387c9c03cb32cc05c36af91f95d
Kim <kimmillena09@gmail.com>
2025-08-01 20:20:47 +0800

    Add dialog modal

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const library = document.querySelector(".library");
+const openFormBtn = document.querySelector(".header-btn");
+const closeFormBtn = document.querySelector(".form-close-btn");
+const dialog = document.querySelector(".modal");
 
 const myLibrary = [];
+
+openFormBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeFormBtn.addEventListener("click", () => {
+  dialog.close();
+});
 
 function Book(title, author, pages, isRead) {
   this.id = crypto.randomUUID();
@@ -15,4 +26,24 @@ function addBookToLibrary(title, author, pages, isRead) {
   myLibrary.push(book);
 }
 
-addBookToLibrary("Title", "Author", 213, "read");
+function displayBooks() {
+  for (const book of myLibrary) {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    const title = document.createElement("h1");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const isRead = document.createElement("p");
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    isRead.textContent = book.isRead;
+    library.appendChild(bookCard);
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(isRead);
+  }
+}
+
+displayBooks();
